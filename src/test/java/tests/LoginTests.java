6b37dc630b0bec2dc108026786823710f3013f62
Shilol Pesahov <pesahov777@gmail.com>
2023-03-16 21:06:58 +0200

    Commit1

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,39 @@
 package tests;
 
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTests extends TestBase{
+public class LoginTests extends TestBase {
+
+    @BeforeMethod
+    public void preCondition(){
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
 
     @Test
-    public void test(){
+    public void loginSuccess() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("shilol@gmail.com", "Shilol12345$");
+        app.getHelperUser().submitLogin();
 
+        //Assert
+        //Assert.assertEquals(,);
+        //Assert.assertNotEquals();
+        //Assert.assertFalse();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
     }
+
+    @Test
+    public void loginSuccessModel() {
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("shilol@gmail.com", "Shilol12345$");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+    }
+
 }
